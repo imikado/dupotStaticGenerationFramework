08f332a07e1ffd7a872d0f8f03db2b71cf5c1ace
imikado@gmail.com <imikado@gmail.com>
2022-11-12 15:07:58 +0100

    modify page setup class

--- src/Page/PageAbstract.php
+++ src/Page/PageAbstract.php
@@ -2,20 +2,9 @@
 
 namespace Dupot\StaticGenerationFramework\Page;
 
-use Exception;
-
 abstract class PageAbstract
 {
-    protected $filename = null;
     protected $paramList = [];
-
-    public function getFilename(): string
-    {
-        if ($this->filename === null) {
-            throw new Exception('missing protected $filename in your Page class');
-        }
-        return $this->filename;
-    }
 
     public function renderLayoutWithParamList(string $layoutPath, array $paramList): string
     {
--- src/Page/PageInterface.php
+++ src/Page/PageInterface.php
@@ -4,5 +4,6 @@ namespace Dupot\StaticGenerationFramework\Page;
 
 interface PageInterface
 {
+    public function getFilename(): string;
     public function render(): string;
 }
